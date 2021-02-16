799da7839530fad69ed845e898263f9645a54d9b
Dairkoos <69522956+HoDairkoos@users.noreply.github.com>
2021-02-16 08:39:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,25 @@
-const Discord = require("discord.js");
-const Enmap = require("enmap");
-const fs = require("fs");
-const client = new Discord.Client({partials: ["MESSAGE", "USER", "REACTION"]});
-const config = require("./config.json");
+const { TOKEN, CHANNEL, SERVER, STATUS, LIVE } = require("./config.json");
+const discord = require("discord.js");
+const client = new discord.Client();
+const ytdl = require('ytdl-core');
 
-const { play } = require("./include/play");
-const {YOUTUBE_API_KEY} = require("./config.json");
-const ytdl = require("ytdl-core");
-const YouTubeAPI = require("simple-youtube-api");
-const youtube = new YouTubeAPI(YOUTUBE_API_KEY);
+client.on('ready', async () => {
+  client.user.setActivity(STATUS + " Dairkoos")
+  let channel = client.channels.cache.get(CHANNEL) || await client.channels.fetch(CHANNEL)
 
-client.queue = new Map();
-client.past = new Map();
+  if(!channel) return;
+  const connection = await channel.join();
+  connection.play(ytdl(LIVE))
+})
 
+setInterval(async function() {
+  if(!client.voice.connections.get(SERVER)) {
+    let channel = client.channels.cache.get(CHANNEL) || await client.channels.fetch(CHANNEL)
+    if(!channel) return;
 
-client.config = config;
-client.points = new Enmap({name: "points"});
-client.settings = new Enmap({
-    name: "settings",
-    autoFetch: true,
-    cloneLevel: "deep",
-    fetchAll: true
-});
-client.confess = new Enmap({name: "confess"})
-client.logger = new Enmap({name: "logger"})
+    const connection = await channel.join()
+    connection.play(ytdl(LIVE))
+  }
+}, 20000)
 
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, event.bind(null, client));
-  });
-});
-
-client.commands = new Enmap();
-
-fs.readdir("./commands/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Tentative de chargement de la commande ${commandName}`);
-    client.commands.set(commandName, props);
-  });
-});
-
-client.login(config.token);
+client.login(TOKEN)
